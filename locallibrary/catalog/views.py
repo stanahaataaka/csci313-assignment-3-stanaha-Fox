8a0c77838a1882d72a03ad564d37e54268dd2fee
tanaha' Fox <xfox2210@gmail.com>
2024-04-03 11:35:02 -0500

    Added tests

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -38,14 +38,14 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 5
+    paginate_by = 10
 
 class BookDetailView(generic.DetailView):
     model = Book
 
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 5
+    paginate_by = 10
 
 class AuthorDetailView(generic.DetailView):
     model = Author
